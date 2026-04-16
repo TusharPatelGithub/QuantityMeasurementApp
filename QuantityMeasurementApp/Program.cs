74c68683ae522a2cf9ee65b0e1255f6692e98cb3
TusharPatelGithub <163450209+TusharPatelGithub@users.noreply.github.com>
2026-04-16 10:26:34 +0530

    Update Program.cs

--- QuantityMeasurementApp/Program.cs
+++ QuantityMeasurementApp/Program.cs
@@ -49,7 +49,8 @@ builder.Services.AddCors(options =>
                 "http://localhost:5500",
                 "http://localhost:5173",
                 "http://127.0.0.1:5173",
-                "null")           // file:// requests send Origin: null
+                "https://tusharpatelgithub.github.io",
+                "null")
               .AllowAnyHeader()
               .AllowAnyMethod();
     });
